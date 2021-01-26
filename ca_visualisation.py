5da1e37d3f28353fbe98addb25e20a3ad005a891
Nuno <nunocalaim@gmail.com>
2021-01-26 11:53:58 +0000

    Added general libraries

---
+++ ca_visualisation.py
@@ -0,0 +1,203 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from moviepy.video.io.ffmpeg_writer import FFMPEG_VideoWriter
+import tqdm, PIL.Image 
+
+def plot_loss(loss_log, loss_log_classes, folder, id_run, target_classes, color_lookup, save=False):
+    plt.figure(figsize=(10, 4))
+    plt.title('Loss history (log10)')
+    plt.plot(np.log10(loss_log), '.', alpha=0.1)
+    for i in range(loss_log_classes.shape[1]):
+        if loss_log_classes.shape[0] > 0:
+            plt.plot(np.log10(loss_log_classes[:, i]), alpha=0.5, label=target_classes[i], color=color_lookup[i].numpy()/255)
+    plt.legend()
+    if save:
+        plt.savefig(folder + '/output/log_loss_{}'.format(id_run))
+    plt.show()
+
+
+class VideoWriter:
+    def __init__(self, filename, fps=30.0, **kw):
+        self.writer = None
+        self.params = dict(filename=filename, fps=fps, **kw)
+
+    def add(self, img):
+        img = np.asarray(img)
+        if self.writer is None:
+            h, w = img.shape[:2]
+            self.writer = FFMPEG_VideoWriter(size=(w, h), **self.params)
+        if img.dtype in [np.float32, np.float64]:
+            img = np.uint8(img.clip(0, 1)*255)
+        if len(img.shape) == 2:
+            img = np.repeat(img[..., None], 3, -1)
+        if len(img.shape) == 3 and img.shape[-1] == 4:
+            img = img[..., :3] * img[..., 3, None]
+        self.writer.write_frame(img)
+
+    def close(self):
+        if self.writer:
+            self.writer.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *kw):
+        self.close()
+
+def make_run_videos(folder, id_run, i_step, TST_EVOLVE, MutateTestingQ, x, ca, color_lookup):
+    with VideoWriter(folder + '/output/Movie_model_{}_{}.mp4'.format(id_run, i_step)) as vid:
+        for i in tqdm.trange(-1, TST_EVOLVE):
+            if MutateTestingQ and i == int(TST_EVOLVE / 2):
+                c_i = x[:, :, :, 0]
+                x = ca.mutate(x, tf.expand_dims(tf.random.shuffle(c_i), -1))
+            image = zoom(tile2d(classify_and_color(ca, x, color_lookup)), scale=4)
+            if i > -1:
+                x = ca(x)
+                image = zoom(tile2d(classify_and_color(ca, x, color_lookup)), scale=4)
+            im = np.uint8(image * 255)
+            im = PIL.Image.fromarray(im)
+            vid.add(np.uint8(im))
+
+def classify_and_color(ca, x, color_lookup):
+    '''
+    input ca: the CA class instance with proper architecture and learned weights
+    input x: a state of the CA. its shape is (batch_size, height, width, no_channels).
+    output: 
+    '''
+    current_image = x[:, :, :, 0]
+    output_ca = ca.classify(x) # x[:, :, :, -NO_CLASSES:] shape = (b, h, w, NO_CLASSES) where we want values 0 if not that number and 1 if that number
+    return color_images(ca, current_image, output_ca, color_lookup)
+
+def color_images(ca, x, output_x, color_lookup, alpha_color=0.3):
+    '''
+    input x grayscale image: its shape is ([batch_size, ]height, width)
+    input output_x: the output of the CA for the image x. its shape is ([batch_size, ]height, width, NO_CLASSES)
+    output: rgba_pixels, with numbers between 0 and 1. its shate is ([b, ]h, w, 4)
+    '''
+    if ca.dp:
+        is_alive = x # 1 if pixel is alive, 0 if background. its shape ([b, ]h, w)
+        is_background = 1. - x # 0 if pixel is alive, 1 if background. its shape ([b, ]h, w)
+        output_x = output_x * tf.expand_dims(is_alive, -1) # forcibly cancels everything outside of it. y_pic will be 0 for dead pixels. and the previous value for the alive pixels
+        
+        black_and_bg = tf.fill(list(x.shape) + [2], 0.01) # an array filled with 0.01 of shape ([b, ]h, w, 2). 0.01 is the minimum value for outputs to be considered
+        black_and_bg *= tf.stack([is_alive, is_background], -1) # an array filled with 0.01 and 0's depending on alive status. of shape ([b, ]h, w, 2)
+
+        number_or_background =  tf.concat([output_x, black_and_bg], -1) # the voting for the NO_CLASSES+2 different categories. (NO_CLASSES colors + original grayscale and background. its shape is ([b, ]h, w, NO_CLASSES+2)
+        classified_pixels = tf.argmax(number_or_background, -1) # the result of the vote.  its shape is ([b, ]h, w)
+        rgb_pixels = tf.gather(color_lookup, classified_pixels) # And now I suppose this shape is ([b, ]h, w, 3)
+        return tf.cast(rgb_pixels, tf.float32) / 255.
+    else:
+        black = tf.fill(list(x.shape) + [1], 0.01) # an array filled with 0.01 of shape ([b, ]h, w, 1). 0.01 is the minimum value for outputs to be considered
+        class_or_black =  tf.concat([output_x, black], -1) # the voting for the NO_CLASSES+1 different categories. (NO_CLASSES colors + undecided. its shape is ([b, ]h, w, NO_CLASSES+1)
+        classified_class = tf.argmax(class_or_black, -1) # the result of the vote.  its shape is ([b, ]h, w)
+        rgb_pixels = tf.gather(color_lookup, classified_class) # And now I suppose this shape is ([b, ]h, w, 3)
+        rgb_pixels = tf.cast(rgb_pixels, tf.float32) / 255.
+        rgb_composite = rgb_pixels * alpha_color + tf.expand_dims(x, -1) * (1 - alpha_color)
+        return rgb_composite
+
+def tile2d(a):
+    '''
+    a has shape (N, H, W, 3), where N is the number of images we will show 
+    '''
+    a = np.asarray(a) # make sure it is an array
+    width = int(np.ceil(np.sqrt(len(a)))) # Number of columns for the tile
+    H, W = a.shape[1:3] # Height and Width of each image
+    pad = (width - len(a)) % width
+    a = np.pad(a, [(0, pad)]+[(0, 0)]*(a.ndim-1), 'constant')
+    height = len(a) // width # Number of rows for the tile
+    a = a.reshape([height, width] + list(a.shape[1:])) # reshaped a to (height, width, H, W, 3)
+    a = np.rollaxis(a, 2, 1) # transform it into shape (height, H, width, W, 3)
+    a = a.reshape([H*height, W*width] + list(a.shape[4:])) # transform into (height * H, width * W, 3)
+    a = np.insert(a, [i*W for i in range(width + 1)], 1, axis=1) # add borders between images
+    a = np.insert(a, [i*H for i in range(height + 1)], 1, axis=0)
+    return a
+
+def zoom(img, scale=4):
+    '''
+    Takes an image array and increases its resolution by simply repeating the values
+    '''
+    img = np.repeat(img, scale, 0)
+    img = np.repeat(img, scale, 1)
+    return img
+
+
+def add_pixel(old_image):
+    new_image = old_image.copy()
+    indices = np.asarray(np.where(old_image == 1)).T
+    success = 0
+    k = 0
+    while (not success) or (k > 100):
+        k += 1
+        idx = np.random.randint(indices.shape[0])
+        x, y = indices[idx, :]
+        x_or_y = np.random.rand() < 0.5
+        pos_or_neg = np.random.rand() < 0.5
+        if x_or_y:
+            if pos_or_neg:
+                dx = 1
+                dy = 0
+            else:
+                dx = -1
+                dy = 0
+        else:
+            if pos_or_neg:
+                dx = 0
+                dy = 1
+            else:
+                dx = 0
+                dy = -1
+        nx = min(max(0, x + dx), 9)
+        ny = min(max(0, y + dy), 9)
+        if old_image[nx, ny] < 0.5:
+            new_image[nx, ny] = 1
+            success = 1
+    return new_image
+
+def class_indice_f(no_pixels, limits_classes):
+    '''
+    find the indice of the class of the no_pixels
+    '''
+    NO_CLASSES = len(limits_classes) + 1
+    indices = np.where(no_pixels <= np.array(limits_classes))[0]
+    if indices.shape[0] == 0:
+        return NO_CLASSES - 1
+    else:
+        return indices[0]
+
+def make_videos_increase(folder, id_run, i_step, TST_EVOLVE, ca, color_lookup, images, InitializeRandomQ, limits_classes):
+    no_steps = images.shape[0]
+    if InitializeRandomQ:
+        x = ca.initialize_random(images[0, :, :, :])
+    else:
+        x = ca.initialize(images[0, :, :, :])
+    with VideoWriter(folder + '/output/Movie_test_increase_{}_{}.mp4'.format(id_run, i_step)) as vid:
+        for j in range(no_steps):
+            x = ca.mutate(x, tf.expand_dims(images[j, :, :, :], -1))
+            for i in tqdm.trange(TST_EVOLVE):
+                x = ca(x)
+                image = zoom(tile2d(classify_and_color(ca, x, color_lookup)), scale=4)
+                im = np.uint8(image * 255)
+                im[:4, :, :] = np.uint8(color_lookup[class_indice_f(j+1, limits_classes), :].numpy())
+                im[-4:, :, :] = np.uint8(color_lookup[class_indice_f(j+1, limits_classes), :].numpy())
+                im[:, :4, :] = np.uint8(color_lookup[class_indice_f(j+1, limits_classes), :].numpy())
+                im[:, -4:, :] = np.uint8(color_lookup[class_indice_f(j+1, limits_classes), :].numpy())
+                im = PIL.Image.fromarray(im)
+                vid.add(np.uint8(im))
+    if InitializeRandomQ:
+        x = ca.initialize_random(images[0, :, :, :])
+    else:
+        x = ca.initialize(images[0, :, :, :])
+    with VideoWriter(folder + '/output/Movie_test_decrease_{}_{}.mp4'.format(id_run, i_step)) as vid:
+        for j in range(no_steps):
+            x = ca.mutate(x, tf.expand_dims(images[no_steps - 1 - j, :, :, :], -1))
+            for i in tqdm.trange(TST_EVOLVE):
+                x = ca(x)
+                image = zoom(tile2d(classify_and_color(ca, x, color_lookup)), scale=4)
+                im = np.uint8(image * 255)
+                im[:4, :, :] = np.uint8(color_lookup[class_indice_f(no_steps-j, limits_classes), :].numpy())
+                im[-4:, :, :] = np.uint8(color_lookup[class_indice_f(no_steps-j, limits_classes), :].numpy())
+                im[:, :4, :] = np.uint8(color_lookup[class_indice_f(no_steps-j, limits_classes), :].numpy())
+                im[:, -4:, :] = np.uint8(color_lookup[class_indice_f(no_steps-j, limits_classes), :].numpy())
+                im = PIL.Image.fromarray(im)
+                vid.add(np.uint8(im))
---
+++ datasets_library.py
@@ -0,0 +1,391 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+from PIL import Image
+import glob
+
+def y2yhot(x, y, NO_CLASSES, task):
+    '''
+    input x shape is (no_images, height, width)
+    input y shape is (no_images,)
+    output: yhot (no_images, height, width, NO_CLASSES) y_res[b, h, w, i] = 1 if the image b belongs to class i
+    '''
+    yhot = np.zeros(list(x.shape) + [NO_CLASSES])
+    for i in range(x.shape[0]):
+        if task == 'count_pixels':
+            yhot[i, :, :, y[i]] = x[i, :, :]
+        else:
+            yhot[i, :, :, y[i]] = np.ones((x.shape[1], x.shape[2]))
+    return yhot.astype(np.float32)
+
+
+
+def get_dataset(task, opt, RebuildDatasetQ, folder):
+
+    ds_str = task
+    SEED_DATASET = opt['SEED_DATASET']
+    if task == 'emotions':
+        classes_labels, DataBalancingQ, DataAugmentationQ = opt['classes_labels'], opt['DataBalancingQ'], opt['DataAugmentationQ']
+        H, W = 48, 48
+        NO_CLASSES = len(classes_labels)
+        tc_str = str(classes_labels).replace('[', '').replace(']', '').replace(', ', '_').replace('\'', '')
+        ds_str += '_' + tc_str
+        if DataBalancingQ:
+            ds_str += '_Bal'
+        if DataAugmentationQ:
+            ds_str += '_Aug'
+    elif task == 'count_pixels':
+        limits_classes, H, W, SIZE_DS, MAXPIXELS, BOUNDARY_P = opt['limits_classes'], opt['H'], opt['W'], opt['SIZE_DS'], opt['MAXPIXELS'], opt['BOUNDARY_P']
+        NO_CLASSES = len(limits_classes) + 1 # Number of classes that the CA must distinguish
+        cb_str = str(limits_classes).replace('[', '').replace(']', '').replace(', ', '_')
+        ds_str += '_ClassB' + cb_str
+        if H != 10 or W != 10:
+            ds_str += '_H{}_W{}'.format(H, W)
+        if SIZE_DS != 100000:
+            ds_str += '_{}'.format(SIZE_DS)
+        if MAXPIXELS != 44:
+            ds_str += '_MaxPixels{}'.format(MAXPIXELS)
+        if BOUNDARY_P != 0.4:
+            ds_str += '_BoundP{}'.format(int(BOUNDARY_P*100))
+        classes_labels = ['pixels<={}'.format(limits_classes[0])] + ['{}<pixels<={}'.format(limits_classes[i], limits_classes[i+1]) for i in range(len(limits_classes) - 1)] + ['{}<pixels<={}'.format(limits_classes[-1], MAXPIXELS)]
+    elif task == 'xor':
+        classes_labels, type_ds, SIZE_DS, obj1_str, obj2_str, H, WW, WW_test = opt['classes_labels'], opt['type_ds'], opt['SIZE_DS'], opt['obj1_str'], opt['obj2_str'], opt['H'], opt['WW'], opt['WW_test']
+        NO_CLASSES = len(classes_labels)
+        if type_ds != 'xor':
+            ds_str += type_ds
+        if SIZE_DS != 10000:
+            ds_str += '_Size{}'.format(SIZE_DS)
+        if obj1_str != 'vertical_small':
+            ds_str += '_Obj1{}'.format(obj1_str)
+        if obj2_str != 'horizontal_small':
+            ds_str += '_Obj2{}'.format(obj2_str)
+        if H != 3:
+            ds_str += '_H{}'.format(H)
+        ds_str += '_Sep{}'.format(WW)
+        ds_str_test = ds_str + '_Sep{}'.format(WW_test)
+        # Prepare the dataset
+        dict_obj = {
+            'vertical_small':
+            np.array([[1], [1], [1]]),
+            'horizontal_small':
+            np.array([[1, 1, 1]]),
+        }
+        t_obj1 = dict_obj[obj1_str]
+        t_obj2 = dict_obj[obj2_str]
+        obj1, obj2 = embed_objs(t_obj1, t_obj2, H)
+        W = 2 * H + WW
+        W_test = 2 * H + WW_test
+        if SEED_DATASET != 1:
+            ds_str_test += '_Seed{}'.format(SEED_DATASET)
+    elif task == 'fruits':
+        SEED_DATASET = 1
+        H, W = opt['H'], opt['W']
+        NO_CLASSES = 3 # Number of classes that the CA must distinguish
+        if H != 30 or W != 30:
+            ds_str += '_H{}_W{}'.format(H, W)
+        classes_labels = ['apple', 'banana', 'orange']
+    elif task == 'frozen_noise':
+        H, W, img_per_class = opt['H'], opt['W'], opt['img_per_class'] 
+        if img_per_class > 1:
+            ds_str += '_ImgPerClass{}'.format(img_per_class)
+        NO_CLASSES = 2 # Number of classes that the CA must distinguish
+        if H != 50 or W != 50:
+            ds_str += '_H{}_W{}'.format(H, W)
+        classes_labels = ['0', '1']
+    if SEED_DATASET != 1:
+        ds_str += '_Seed{}'.format(SEED_DATASET)
+
+    BuildDS = False
+    if RebuildDatasetQ:
+        BuildDS = True
+    else:
+        try:
+            res = np.load(folder + '/dataset/{}/{}.npz'.format(task, ds_str))
+            x_train, x_test, y_train, y_test = res['x_train'], res['x_test'], res['y_train'], res['y_test']
+        except:
+            BuildDS = True
+    if BuildDS:
+
+        if task == 'count_pixels':
+            x_train, x_test, y_train, y_test = build_dataset_count_pixels(SIZE_DS, H, W, limits_classes, max_pixels=MAXPIXELS, boundary_p=BOUNDARY_P, seed=SEED_DATASET)
+        elif task == 'xor':
+            x_train, x_test, y_train, y_test = build_dataset_xor(SIZE_DS, obj1, obj2, WW, type_ds=type_ds, seed=SEED_DATASET)
+        elif task == 'emotions':
+            x_train, x_test, y_train, y_test = build_dataset_emotions(folder, classes_labels, H, W, DataBalancingQ, DataAugmentationQ, seed=SEED_DATASET)
+        elif task == 'fruits':
+            x_train, x_test, y_train, y_test = build_dataset_fruits(folder, H, W)
+        elif task == 'frozen_noise':
+            x_train, x_test, y_train, y_test = build_dataset_frozen_noise(folder, H, W, img_per_class, seed=SEED_DATASET)
+        print(y_train)        
+        np.savez(folder + '/dataset/{}/{}.npz'.format(task, ds_str), x_train=x_train, x_test=x_test, y_train=y_train, y_test=y_test)
+        if task == 'xor':
+            x_train_ignore, x_test, y_train_ignore, y_test = build_dataset_xor(SIZE_DS, obj1, obj2, WW_test, type_ds=type_ds, seed=SEED_DATASET)
+            np.savez(folder + '/dataset/{}/{}.npz'.format(task, ds_str_test), x_train=x_train_ignore, x_test=x_test, y_train=y_train_ignore, y_test=y_test)
+
+
+    cols=7
+    rows=7
+    fig, ax = plt.subplots()
+    DISP = np.zeros((1, cols * W + 1))
+    RES = [[0 for i in range(cols)] for j in range(rows)]
+    for i in range(rows):
+        disp = np.zeros((H, 1))
+        for j in range(cols):
+            image_idx = np.random.randint(x_train.shape[0])
+            disp = np.hstack((disp, x_train[image_idx, :, :]))
+            RES[i][j] = y_train[image_idx]
+        DISP = np.vstack((DISP, disp))
+    ax.imshow(DISP)
+    print(np.array(RES))
+
+
+    return x_train, x_test, y_train, y_test, ds_str, NO_CLASSES, H, W, classes_labels
+
+
+def class_indice_f(no_pixels, limits_classes):
+    '''
+    find the indice of the class of the no_pixels
+    '''
+    NO_CLASSES = len(limits_classes) + 1
+    indices = np.where(no_pixels <= np.array(limits_classes))[0]
+    if indices.shape[0] == 0:
+        return NO_CLASSES - 1
+    else:
+        return indices[0]
+
+
+def new_image(no_pixels, H, W):
+    X = np.zeros((H, W), np.float32)
+    pixels_placed = 1
+    first_pixel = (int(H/2), int(W/2)) # we insert the first pixel at the center of the image
+    positions_colored = [first_pixel]
+    X[first_pixel[0], first_pixel[1]] = 1
+    while pixels_placed < no_pixels:
+        base_pixel = random.choice(positions_colored)
+        x, y = base_pixel[0], base_pixel[1]
+        if np.random.rand() < 0.5:
+            if np.random.rand() < 0.5:
+                dx = 1
+                dy = 0
+            else:
+                dx = -1
+                dy = 0
+        else:
+            if np.random.rand() < 0.5:
+                dx = 0
+                dy = 1
+            else:
+                dx = 0
+                dy = -1
+        test_pixel = (min(max(0, x + dx), H-1), min(max(0, y + dy), W-1))
+        if test_pixel not in positions_colored:
+            pixels_placed += 1
+            positions_colored.append(test_pixel)
+            X[test_pixel[0], test_pixel[1]] = 1
+    return X        
+
+
+def build_dataset_count_pixels(size_ds, H, W, limits_classes, max_pixels=20, boundary_p=0.5, seed=1):
+    '''
+    This function creates a balanced dataset, with special incidence for boundary conditions
+    '''
+    X = np.zeros((size_ds, H, W), np.float32)
+    Y = np.zeros((size_ds), np.int)
+    np.random.seed(seed)
+    for i in range(size_ds):
+        if np.random.rand() < boundary_p:
+            no_pixels = np.random.choice(limits_classes)
+            if np.random.rand() < 0.5:
+                no_pixels += 1
+        else:
+            no_classes = len(limits_classes) + 1
+            class_i = np.random.choice(no_classes)
+            if class_i == 0:
+                no_pixels = np.random.randint(limits_classes[0]) + 1
+            elif class_i == no_classes - 1:
+                no_pixels = np.random.randint(limits_classes[-1], max_pixels) + 1
+            else:
+                no_pixels = np.random.randint(limits_classes[class_i-1], limits_classes[class_i]) + 1
+        X[i, :, :] = new_image(no_pixels, H, W)
+        Y[i] = class_indice_f(no_pixels, limits_classes)
+    x_train, x_test = np.split(X, [int(size_ds*0.8)])
+    y_train, y_test = np.split(Y, [int(size_ds*0.8)])
+    return x_train, x_test, y_train, y_test
+
+
+def embed_objs(t_obj1, t_obj2, H):
+    obj1 = np.zeros((H, H))
+    h1, w1 = t_obj1.shape
+    h_i, w_i = int((H - h1) / 2), int((H - w1) / 2)
+    obj1[h_i:(h_i + h1), w_i:(w_i + w1)] = t_obj1
+    obj2 = np.zeros((H, H))
+    h2, w2 = t_obj2.shape
+    h_i, w_i = int((H - h2) / 2), int((H - w2) / 2)
+    obj2[h_i:(h_i + h2), w_i:(w_i + w2)] = t_obj2
+    return obj1, obj2
+
+def build_dataset_xor(size_ds, obj1, obj2, WW, type_ds='xor', seed=1):
+    H, W = obj1.shape
+    np.random.seed(seed)
+    X = np.zeros((size_ds, H, 2 * W + WW), np.float32)
+    Y = np.zeros((size_ds), np.int)
+
+    for i in range(size_ds):
+        if type_ds == 'xor':
+            if np.random.rand() < 0.5:
+                X[i, :, :W] = obj1
+                j = 0
+            else:
+                X[i, :, :W] = obj2
+                j = 1
+            if np.random.rand() < 0.5:
+                X[i, :, -W:] = obj1
+                Y[i] = 1 - j
+            else:
+                X[i, :, -W:] = obj2
+                Y[i] = j
+        elif type_ds == 'simple':
+            if np.random.rand() < 0.5:
+                X[i, :, :W] = obj1
+                X[i, :, -W:] = obj1
+                Y[i] = 0
+            else:
+                X[i, :, :W] = obj2
+                X[i, :, -W:] = obj2
+                Y[i] = 1
+    x_train, x_test = np.split(X, [int(size_ds*0.8)])
+    y_train, y_test = np.split(Y, [int(size_ds*0.8)])
+    return x_train, x_test, y_train, y_test
+
+
+def build_dataset_emotions(folder, classes_labels, H, W, DataBalancingQ, DataAugmentationQ, seed=1):
+    with open(folder + '/dataset/emotions/emotion_dataset.npy', 'rb') as f:
+        X = np.load(f)
+        y = np.load(f)
+    original_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+    class_labels = [original_labels.index(classes_labels[i]) for i in range(len(classes_labels))]
+
+    X_final = np.zeros((0, H, W))
+    y_final = np.zeros((0,))
+    if DataBalancingQ:
+        max_class = 0
+        for new_class, class_label in enumerate(class_labels):
+            idx = np.where(y == class_label)[0]
+            max_class = max(max_class, idx.shape[0])
+
+    for new_class, class_label in enumerate(class_labels):
+
+        X_sub = np.zeros((0, H, W))
+        y_sub = np.zeros((0,))
+
+        idx = np.where(y == class_label)[0]
+        len_class = idx.shape[0]
+
+        X_sub = np.concatenate((X_sub, X[idx, :, :, 0]))
+        y_sub = np.concatenate((y_sub, new_class * np.ones(len_class)))
+        if DataAugmentationQ:
+            X_sub = np.concatenate((X_sub, X[idx, :, ::-1, 0]))
+            y_sub = np.concatenate((y_sub, new_class * np.ones(len_class)))
+            X_sub = np.concatenate((X_sub, X[idx, :, :, 0] / 2))
+            y_sub = np.concatenate((y_sub, new_class * np.ones(len_class)))
+            X_sub = np.concatenate((X_sub, X[idx, :, ::-1, 0] / 2))
+            y_sub = np.concatenate((y_sub, new_class * np.ones(len_class)))
+            X_sub = np.concatenate((X_sub, np.clip(X[idx, :, :, 0] * 1.2, 0, 255)))
+            y_sub = np.concatenate((y_sub, new_class * np.ones(len_class)))
+            X_sub = np.concatenate((X_sub, np.clip(X[idx, :, ::-1, 0] * 1.2, 0, 255)))
+            y_sub = np.concatenate((y_sub, new_class * np.ones(len_class)))
+            if DataBalancingQ:
+                remaining_items = 6 * (max_class - len_class)
+                idx_rnd = np.random.choice(idx, remaining_items)
+                X_sub = np.concatenate((X_sub, X[idx_rnd, :, :, 0]))
+                y_sub = np.concatenate((y_sub, new_class * np.ones(remaining_items)))
+        elif DataBalancingQ:
+            remaining_items = max_class - len_class
+            idx_rnd = np.random.choice(idx, remaining_items)
+            X_sub = np.concatenate((X_sub, X[idx_rnd, :, :, 0]))
+            y_sub = np.concatenate((y_sub, new_class * np.ones(remaining_items)))
+
+        X_final = np.concatenate((X_final, X_sub))
+        y_final = np.concatenate((y_final, y_sub))
+    
+    X_final /= 255
+    X_final = X_final.astype(np.float32)
+    y_final = y_final.astype(np.int)
+    size_ds = y_final.shape[0]
+    np.random.seed(seed)
+    rnd_idx = np.random.permutation(size_ds)
+    x_train, x_test = np.split(X_final[rnd_idx, :, :], [int(size_ds*0.8)])
+    y_train, y_test = np.split(y_final[rnd_idx], [int(size_ds*0.8)])
+    return x_train, x_test, y_train, y_test
+
+
+
+def build_dataset_fruits(folder, H, W):
+    imgstr = '/dataset/fruits'
+
+    x_train = np.zeros((0, W, H))
+    y_train = np.zeros((0,), dtype=np.int)
+    def create_image(filepath):
+        img = Image.open(filepath).convert('L')
+        w, h = img.size
+        mwh = min(w, h)
+        left = (w - mwh)//2
+        top = (h - mwh)//2
+        img = img.crop((left, top, left+mwh, top+mwh))
+        img = img.resize((W,H))
+        img_np = np.asarray(img).reshape((1, W, H))
+        return img_np
+    for filepath in glob.iglob(folder + imgstr + '/train/apple*.jpg'):
+        img_np = create_image(filepath)
+        x_train = np.vstack((x_train, img_np))
+        y_train = np.concatenate((y_train, np.array([0])))
+    for filepath in glob.iglob(folder + imgstr + '/train/banana*.jpg'):
+        img_np = create_image(filepath)
+        x_train = np.vstack((x_train, img_np))
+        y_train = np.concatenate((y_train, np.array([1])))
+    for filepath in glob.iglob(folder + imgstr + '/train/orange*.jpg'):
+        img_np = create_image(filepath)
+        x_train = np.vstack((x_train, img_np))
+        y_train = np.concatenate((y_train, np.array([2])))
+    x_test = np.zeros((0, W, H))
+    y_test = np.zeros((0,), dtype=np.int)
+    for filepath in glob.iglob(folder + imgstr + '/test/apple*.jpg'):
+        img_np = create_image(filepath)
+        x_test = np.vstack((x_test, img_np))
+        y_test = np.concatenate((y_test, np.array([0])))
+    for filepath in glob.iglob(folder + imgstr + '/test/banana*.jpg'):
+        img_np = create_image(filepath)
+        x_test = np.vstack((x_test, img_np))
+        y_test = np.concatenate((y_test, np.array([1])))
+    for filepath in glob.iglob(folder + imgstr + '/test/orange*.jpg'):
+        img_np = create_image(filepath)
+        x_test = np.vstack((x_test, img_np))
+        y_test = np.concatenate((y_test, np.array([2])))
+    for filepath in glob.iglob(folder + imgstr + '/test/mixed*.jpg'):
+        img_np = create_image(filepath)
+        x_test = np.vstack((x_test, img_np))
+        y_test = np.concatenate((y_test, np.array([2])))
+    x_train /= 255
+    x_test /= 255
+    x_train = x_train.astype(np.float32)
+    x_test = x_test.astype(np.float32)
+    y_train = y_train.astype(np.int)
+    y_test = y_test.astype(np.int)
+
+    return x_train, x_test, y_train, y_test
+
+
+
+def build_dataset_frozen_noise(folder, H, W, img_per_class, seed=1):
+    x_train = np.zeros((2 * img_per_class, W, H))
+    y_train = np.zeros((2 * img_per_class,), dtype=np.int)
+    np.random.seed(seed)
+    for i in range(img_per_class):
+        x_train[2*i, :, :] = np.random.rand(H, W)
+        x_train[2*i+1, :, :] = np.random.rand(H, W)
+        y_train[2*i] = 0
+        y_train[2*i+1] = 1
+    x_train = x_train.astype(np.float32)
+    y_train = y_train.astype(np.int)
+
+    return x_train, x_train, y_train, y_train
